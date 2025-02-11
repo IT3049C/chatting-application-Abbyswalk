ca8bd34537ab1fd3baff14d861b1d4f8d3233514
Abbyswalk <aswalker205@gmail.com>
2025-02-11 09:26:27 -0500

    css

---
+++ resources/js/index.js
@@ -0,0 +1,5 @@
+const nameInput = document.getElementById('my-name-input');
+const messageInput = document.getElementById('my-message');
+const sendButton = document.getElementById('send-button');
+const chatBox = document.getElementById('chat');
+
